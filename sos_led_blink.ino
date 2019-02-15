6ac4a77a6b024f4efe8d042dd4d9a75e7c834af7
hallan2 <47231937+hallan2@users.noreply.github.com>
2019-02-15 13:14:49 -0500

    Add files via upload

---
+++ sos_led_blink.ino
@@ -0,0 +1,27 @@
+int ledPin=10;
+
+ void setup() {
+  // put your setup code here, to run once:
+pinMode(ledPin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+    flash(200); flash(200); flash(200); // S, 3 short flashes
+    delay(600); //3 dots between letters
+    flash(500); flash(500); flash(500); // O, 3 long flashes
+    delay(600); //3 dots between letters
+    flash(200); flash(200); flash(200); // S, 3 short flashes
+    delay(1000);
+    
+}
+
+void flash(int period) 
+{
+  digitalWrite(ledPin,HIGH); //led on
+  delay(period); // period of time on depending on letter
+  digitalWrite(ledPin,LOW); //led off
+  delay(period); //period of time off depending on letter
+}
+
